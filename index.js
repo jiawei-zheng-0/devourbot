8927cb29658d67455572f396a46201a8365c8fd8
Jia Wei Zheng <jiasterop@gmail.com>
2019-01-15 17:00:19 -0500

    Restrict commands to devour members

--- index.js
+++ index.js
@@ -8,20 +8,27 @@ client.once('ready', () => {
 
 client.on('message', message => {
     console.log(message.content);
+    if (message.channel.type === 'text') {// message in text channel
+        if (message.member.roles.exists('name', 'Devour')) {// message from a devour member
+            console.log('Devour Member');
+            if (message.content === '$balance') {
+                console.log('Balance request from ' + message.author.username + 'id = ' + message.author.id);
+                message.author.send('Your balance for this week is ' + message.channel.name);
+            }
+            else if (message.content === '$guildbalance') {
+                console.log('Guild total request from ' + message.author.id);
+                message.author.send('Guild total for this week is ' + message.channel.name);
+            }
+        }
+        else {
+            console.log('Non Devour Member');// message not from a devour member
+        }
+    }
     /*
     if (message.content.includes('loli')) {
         message.channel.send('L O L I S :POGGERS:');
     }
     */
-    if (message.content === '$balance') {
-        console.log('Balance request from ' + message.author.username + 'id = ' + message.author.id);
-        message.author.send('Your balance for this week is ');
-    }
-    else if (message.content === '$guildbalance') {
-        console.log('Guild total request from ' + message.author.id);
-        message.author.send('Guild total for this week is ');
-}
-
 });
 
 client.login(config.token);
