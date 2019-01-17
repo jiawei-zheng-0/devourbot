5db2d54f3226ce5ee96d979f88a84883c45103a8
Jia Wei Zheng <jiasterop@gmail.com>
2019-01-16 20:36:53 -0500

    Finish get individual balance and Start summary functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const readline = require('readline');
 const { google } = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -108,6 +108,75 @@ function getGuildBalance(auth, callback) {
         }
     });
 }
+function getBalanceByID(auth, id, callback) {
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    const resource = {
+        'values': [
+            [
+                id
+            ]
+        ]
+    };
+    sheets.spreadsheets.values.update({
+        spreadsheetId: config.sheetID,
+        range: config.idCell,
+        valueInputOption: 'USER_ENTERED',
+        resource: resource
+    }, (err, res) => {
+        if (err) console.log('The API returned an error: ' + err);
+        //var result = res.result;
+        //console.log(result);
+        sheets.spreadsheets.values.get({
+            spreadsheetId: config.sheetID,
+            range: config.familyNameCell,
+        }, (err, res) => {
+            if (err) callback('The API returned an error: ' + err);
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log('Data: ' + rows[0][0]);
+                callback(null, rows[0][0]);
+            } else {
+                console.log('No data found.');
+            }
+        });
+    });
+}
+
+function getSummary(auth, id, callback) {
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    const resource = {
+        'values': [
+            [
+                id
+            ]
+        ]
+    };
+    sheets.spreadsheets.values.update({
+        spreadsheetId: config.sheetID,
+        range: config.summaryIDCell,
+        valueInputOption: 'USER_ENTERED',
+        resource: resource
+    }, (err, res) => {
+        if (err) console.log('The API returned an error: ' + err);
+        //var result = res.result;
+        //console.log(result);
+        sheets.spreadsheets.values.get({
+            spreadsheetId: config.sheetID,
+            range: config.summaryRange,
+        }, (err, res) => {
+            if (err) callback('The API returned an error: ' + err);
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log('Data: ' + rows[0]);
+                callback(null, rows[0]);
+            } else {
+                console.log('No data found.');
+            }
+        });
+    });
+}
 
 client.once('ready', () => {
     console.log('Start');
@@ -117,9 +186,12 @@ client.on('message', message => {
     if (message.channel.type === 'text') {// message in text channel
         console.log('Message Recieved: ' + message.author.username + ': ' + message.content);
         if (message.content === '$balance') {
-            if (message.member.roles.some('name', 'Devour')) {// message from a devour member
+            if (message.member.roles.exists('name', 'Devour')) {// message from a devour member
                 console.log('Request: Balance request from ' + message.author.username + 'id = ' + message.author.id);
-                message.author.send('Your balance for this week is ' + message.channel.name);
+                getBalanceByID(key, message.author.id, function (err, data) {
+                    message.author.send('Your balance for this week is ' + data);
+                    console.log('Sent Message to ' + message.author.id + ': Your for this week is ' + data);
+                });
             }
             else {
                 console.log('Error: Non Devour Member');// message not from a devour member
@@ -128,9 +200,49 @@ client.on('message', message => {
         else if (message.content === '$guildbalance') {
             if (message.member.roles.exists('name', 'Devour')) {// message from a devour member
                 console.log('Request: Guild total request from ' + message.author.username + ' id = ' + message.author.id);
-                getGuildBalance(key, function(err, data) {
+                getGuildBalance(key, function (err, data) {
                     message.author.send('Guild total for this week is ' + data);
                     console.log('Sent Message to ' + message.author.id + ': Guild total for this week is ' + data);
+                });
+            }
+            else {
+                console.log('Error: Non Devour Member');// message not from a devour member
+            }
+        }
+        else if (message.content === '$summary') {
+            if (message.member.roles.exists('name', 'Devour')) {// message from a devour member
+                console.log('Request: Summary request from ' + message.author.username + ' id = ' + message.author.id);
+                getSummary(key, message.author.id, function(err, data) {
+                    data.forEach(e => {
+                        if (e == '') e = 0;
+                    });
+                    var summaryText = 'Time: ' + data[0]
+                        + '\nFamily Name: ' + data[1]
+                        + '\nDeckhand 1 Family Name: ' + data[2]
+                        + '\nDeckhand 2 Family Name: ' + data[3]
+                        + '\nDeckhand 3 Family Name: ' + data[4]
+                        + '\nDeckhand 4 Family Name: ' + data[5]
+                        + '\nTotal Value: ' + data[22]
+                        + '\nSplit amount: ' + data[23]
+                        + '\nSea Monster Neidans: ' + data[6]
+                        + '\nOcean Stalker\'s Skin: ' + data[7]
+                        + '\nOcean Stalker Whiskers: ' + data[8]
+                        + '\nHekaru\'s Spike: ' + data[9]
+                        + '\nAmethyst Hekaru Spikes: ' + data[10]
+                        + '\nCandidum Shells: ' + data[11]
+                        + '\nSTEEL Candidum Shells: ' + data[12]
+                        + '\nNineshark\'s Horns Fragments: ' + data[13]
+                        + '\nNineshark Fins: ' + data[14]
+                        + '\nBlack Rust Jawbones: ' + data[15]
+                        + '\nBlack Rust Tongues: ' + data[16]
+                        + '\nGoldmont Pirate Coins: ' + data[17]
+                        + '\nGoldmont Pirate Goblets: ' + data[18]
+                        + '\nScreenshot of inventory items: ' + data[19]
+                        + '\nScreenshot of guild funds BEFORE turn in: ' + data[20]
+                        + '\nScreenshot of guild funds AFTER turn in: ' + data[21];
+
+                    message.author.send('Last trip: ' + summaryText);
+                    console.log('Sent Message to ' + message.author.id + ': Last trip: ' + summaryText);
                 });
             }
             else {
@@ -144,6 +256,8 @@ client.on('message', message => {
     }
     */
 });
-client.on('error', console.error);
+client.on('error', () => {
+    console.log('Connection reset');
+});
 
 client.login(config.token);
